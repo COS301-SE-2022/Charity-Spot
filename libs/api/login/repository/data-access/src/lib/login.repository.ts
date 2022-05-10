f913c917e5d6278cc88872cd2f8db77888b19976
David Roodt <davidroodt2@gmail.com>
2022-05-10 17:42:21 +0200

    added Login gets/sets

--- libs/api/login/repository/data-access/src/lib/login.repository.ts
+++ libs/api/login/repository/data-access/src/lib/login.repository.ts
@@ -5,11 +5,56 @@ import { PrismaService } from '@charity-spot/api/shared/services/prisma';
 export class LoginRepository {
   constructor(private prisma: PrismaService) {}
 
-  async getNames(){
+  async emailExists(email : string)
+  {
+    const list = await this.prisma.user.findMany({
+        select:
+        {
+            email: true
+        }
+    });
+    list.forEach(i => {
+        if (i.email==email)
+        {
+            return true;
+        }
+    });
+    return false;
+  }
 
-    const users = await this.prisma.user.findMany();
+  async validateLogin(email : string,password: string)
+  {
+    const u = await this.prisma.user.findMany({
+        select:
+        {
+          userID: true,
+        },
+        where:
+        {
+          email: email,
+          password: password
+        }
+    });
+    if(u == null)
+    {
+      return null;
+    }
+    return u.userID;
+  }
 
-    return users;
+  async getSalt(email:string){
+    const salt = await this.prisma.user.fidnFirst({
+      select:
+      {
+        email:email
+      },
+      where:
+      {
+        email: email
+      }
+    });
+
+    return salt;
   }
 
 }
