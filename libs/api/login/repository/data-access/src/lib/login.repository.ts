73fca1a083016f0d87edaf3b649c581f6a94ed61
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-05-11 19:39:01 +0200

    Small fixes

--- libs/api/login/repository/data-access/src/lib/login.repository.ts
+++ libs/api/login/repository/data-access/src/lib/login.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '@charity-spot/libs/api/shared/services/prisma';
+import { PrismaService } from '@charity-spot/api/shared/services/prisma';
 
 @Injectable()
 export class LoginRepository {
