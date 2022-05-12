87306e9de8a26daf16290b2ef7bf500bdd59bead
MoloTheSniper <85521815+MoloTheSniper@users.noreply.github.com>
2022-05-12 23:06:55 +0200

    Incomplete Profile page

--- libs/client/organisation-profile/src/lib/client-organisation-profile.tsx
+++ libs/client/organisation-profile/src/lib/client-organisation-profile.tsx
@@ -1,12 +1,14 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import './profile.css'
 //import userprofile from'../assets/userprofile.png'
 import userprofile from '../../../shared/assets/userprofile.png'
 
 import ListGroup from 'react-bootstrap/esm/ListGroup';
+let fullCookie = document.cookie.split("="); 
+ let IdCookie = fullCookie[1];
 
 async function APICall(usrID:string){
-
+    
   const query = (`query {
     OrgProfile(userID:"${usrID}"){
       Email
@@ -28,24 +30,51 @@ async function APICall(usrID:string){
              },
              body: JSON.stringify({
                query
+
              })
-          }).then(r => r.json()).then(data => 
+          }).then(r => r.json())
+          .then((data) => 
             All_data = data
             );
-    
+            
          return JSON.stringify(All_data);
+         console.log(All_data);
 
 }
+let Loc = '';
 export function Home() {
-
-  const hanndlesubmit = (event: { preventDefault: () => void; }) =>{
+  let [setOEmail,OEmail] = useState('');
+  let [setOName,OName] = useState('');
+  let [setODate,ODate] = useState('');
+  let [setOLocation,OLocation] = useState('');
+  let hanndlesubmit = (event: { preventDefault: () => void; }) =>
+  {
     event.preventDefault();
-}
+  }
+  const displayData = async() =>{
 
+    const response = JSON.parse(await APICall(IdCookie));
+   console.log(response.data.OrgProfile);
+   let allData = response.data.OrgProfile;
+   const {Email,Name,Date,Location} = allData;
+   OEmail = Email;
+   OName = Name;
+   ODate = Date;
+   OLocation = Location;
+   Loc = Location
+   console.log(OLocation);
+
+  }
+  useEffect(() => {
+    displayData();
+   },[]);
+   const response = JSON.stringify(APICall(IdCookie));
+     console.log(response);
+  
   return (
     <div className='profile-page'>
       <br/>
-        <h1 style ={{color: '#1458b3'}}>Profile</h1>
+        <h1 style ={{color: '#1458b3'}}>{IdCookie}</h1>
         <div className='profile-main'>
 
           <div className='user-left'>
@@ -59,7 +88,7 @@ export function Home() {
           <div className='user-right'>
             <br/><br/>
           <ListGroup variant="flush" >
-            <ListGroup.Item style={{ backgroundColor: 'transparent', height: '70px', color: '#104283' }}>Type: Organisation</ListGroup.Item>
+            <ListGroup.Item style={{ backgroundColor: 'transparent', height: '70px', color: '#104283' }}></ListGroup.Item>
             <ListGroup.Item style={{ backgroundColor: 'transparent', height: '70px', color: '#104283' }}>Email: Company1@gmail.com</ListGroup.Item>
             <ListGroup.Item style={{ backgroundColor: 'transparent', height: '70px' , color: '#104283'}}>Name: Company One</ListGroup.Item>
             <ListGroup.Item style={{ backgroundColor: 'transparent', height: '70px' , color: '#104283'}}>Date Registered: 05 May 2022</ListGroup.Item>
