44074b377da545b8a9056caa9e8de54332ca379a
David Roodt <davidroodt2@gmail.com>
2022-05-24 15:49:51 +0200

    Donate get Items

--- libs/api/donate/repository/data-access/src/lib/donate.repository.ts
+++ libs/api/donate/repository/data-access/src/lib/donate.repository.ts
@@ -5,4 +5,62 @@ import { PrismaService } from '@charity-spot/api/shared/services/prisma';
 export class DonateRepository {
   constructor(private prisma: PrismaService) {}
 
+  async getItemList(uID : string)
+  {
+    return await this.prisma.donoItem.findMany({
+      where:
+      {
+        OrgID: uID,
+      },
+      select:
+      {
+        ItemName: true
+      }
+    })
+  }
+
+  async getItemDescription(uID : string, iName: string)
+  {
+    return await this.prisma.donoItem.findFirst({
+      where:
+      {
+        OrgID: uID,
+        ItemName: iName,
+      },
+      select:
+      {
+        Descrition: true
+      }
+    })
+  }
+
+  async getItemQuantity(uID : string, iName: string)
+  {
+    return await this.prisma.donoItem.findFirst({
+      where:
+      {
+        OrgID: uID,
+        ItemName: iName,
+      },
+      select:
+      {
+        Quantity: true
+      }
+    })
+  }
+
+  async getItemQuality(uID : string, iName: string)
+  {
+    return await this.prisma.donoItem.findFirst({
+      where:
+      {
+        OrgID: uID,
+        ItemName: iName,
+      },
+      select:
+      {
+        Quality : true
+      }
+    })
+  }
 }
