2688f282c20ee2a5996eacc40c0217927d922214
MoloTheSniper <85521815+MoloTheSniper@users.noreply.github.com>
2022-05-24 23:40:01 +0200

    Styled Tabs part 2

--- libs/client/home/src/lib/client-home.tsx
+++ libs/client/home/src/lib/client-home.tsx
@@ -1,35 +1,52 @@
-// import styles from './cli';
-import 'react-tabs/style/react-tabs.css';
+import styles from './client-home.module.css';
 import {Tabs,Tab} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import { FaMapMarkerAlt,FaFilter } from 'react-icons/fa'
-
+import { FaTrash,FaBlog,FaCode,FaSave,FaMapMarkerAlt,FaFilter } from 'react-icons/fa'
+import './homee.css';
 
 export function Home() {
   return (
     <div>
-<br/>
 
- <Tabs variant="pills" defaultActiveKey="profile" id="tabss" className="elon" style={{color:"grey"}}>
-  <Tab  eventKey="home" title={
-   <span>
-     <FaMapMarkerAlt />
-     {" Map "}
-   </span>
-  }>
-     <h1>First Tab</h1>
-     {/* <img className={styles['container']} src="https://firebasestorage.googleapis.com/v0/b/charityspotdemo1.appspot.com/o/g1.png?alt=media&token=888e1913-ec8e-4508-b70a-69b590a27587"></img> */}
-  </Tab>
-  <Tab  eventKey="filter" title={
-   <span>
-     {/* <FaFilter />{" Filter "} */}
-   </span> <h1></h1>
-  }>
-  </Tab>
-  <Tab eventKey="contact" title="Contact" >
-      <h1>Third Tab</h1>
-  </Tab>
-</Tabs>   
+      {/* <Tabs>
+            <TabList>
+              <Tab>Map</Tab>
+              <Tab>Filters</Tab>
+            </TabList>
+
+            <TabPanel>
+              <img className={styles['container']} src="https://firebasestorage.googleapis.com/v0/b/charityspotdemo1.appspot.com/o/g1.png?alt=media&token=888e1913-ec8e-4508-b70a-69b590a27587"></img>
+            </TabPanel>
+            <TabPanel>
+              <h2>Select Filters</h2>
+            </TabPanel>
+      
+    
+      
+
+      </Tabs> */}
+     <div className="wrapper">
+    <br/><br/>
+      <input type ="radio" name="slider" id='home' ></input>
+      <input type ="radio" name="slider" id='blog' ></input>
+      {/* <input type ="radio" name="slider" id='code' ></input>
+      <input type ="radio" name="slider" id='help' ></input> */}
+      <nav>
+        <label htmlFor= "home" className='home'><FaMapMarkerAlt/> Map  </label>
+        <label htmlFor= "blog" className='blog'> <FaFilter/> Filter </label>
+        <div className='slider'></div>
+      </nav>
+      <section>
+        <div className='content content-1'>
+          <div className='title'><h1>Content 1</h1></div>
+          <img className={styles['container']} src="https://firebasestorage.googleapis.com/v0/b/charityspotdemo1.appspot.com/o/g1.png?alt=media&token=888e1913-ec8e-4508-b70a-69b590a27587"></img>
+        </div>
+        <div className='content content-2'>
+          <div className='title'><h1>Content 2</h1></div>
+          <p> who was the company's public face. </p>
+        </div>
+      </section>
+    </div>   
     </div>
   );
 }
