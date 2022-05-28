ef5fc8460cf18781dd9ae41a542101d4b7ff2f50
deamon-death <u20451696@tuks.co.za>
2022-05-28 21:09:09 +0200

    🚧 (API + SERVICE) : donate page resolver and service layer

--- libs/api/donate/api/feature/src/lib/donate-resolver.ts
+++ libs/api/donate/api/feature/src/lib/donate-resolver.ts
@@ -1,13 +1,57 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
 import { DonateEntity } from '@charity-spot/api/donate/service/feature';
 import { DonateService } from '@charity-spot/api/donate/service/feature';
+import { catagory, quality } from '@prisma/client';
 
 @Resolver()
 export class DonateResolver {
     constructor(private readonly DonateService: DonateService) {}
 
     @Query(() => DonateEntity)
-    testQ() : Promise<DonateEntity>{
-        return this.DonateService.coolFunc();
+    donate(
+        @Args("userID") id: string,
+        @Args("name") name: string,
+        @Args("quantity") quantity: number,
+        @Args("category") category: any,
+        @Args("condition") condition: any,
+        @Args("descr") descr: string
+    ) {
+        category = (category) : catagory => {
+            switch(category) {
+                case 'Food':
+                    return 'FOOD';
+                case 'Clothes':
+                    return 'CLOTHING';
+                case 'Tech':
+                    return 'TECH';
+                case 'Hygiene':
+                    return 'TOILETRIES';
+                case 'Stationary':
+                    return 'STATIONARY';
+                case 'Furniture':
+                    return 'FURNITURE'
+                case 'Kitchen':
+                    return 'KITCHEN';
+
+                default:
+                    return null;
+            }
+        }
+
+        //Moloko: fix your values in donate page form = donate-box4
+
+        condition = (condition) : quality => {
+            switch(condition) {
+                case 'New':
+                    return 'NEW';
+                case 'Used':
+                    return 'USED';
+
+                default:
+                    return null;
+            }
+        }
+
+        return this.DonateService.donate(id, name, quantity, category, condition, descr);
     }
 }
