ed457b164ddd382b7e59357e20c9bc6555364f49
Elphus (Native) Malope <u20451696@tuks.co.za>
2022-05-29 15:48:09 +0200

    Update donate-resolver.ts

--- libs/api/donate/api/feature/src/lib/donate-resolver.ts
+++ libs/api/donate/api/feature/src/lib/donate-resolver.ts
@@ -34,7 +34,7 @@ export class DonateResolver {
                     return 'KITCHEN';
 
                 default:
-                    return 'FOOD';
+                    return null;
             }
         }
 
