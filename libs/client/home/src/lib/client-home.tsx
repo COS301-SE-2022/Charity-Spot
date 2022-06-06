9f35ae8815cad1e90f580122da9fdf40cc23e5bf
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-06-06 16:04:28 +0200

    Hide Google Maps Key

--- libs/client/home/src/lib/client-home.tsx
+++ libs/client/home/src/lib/client-home.tsx
@@ -4,11 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaTrash,FaBlog,FaCode,FaSave,FaMapMarkerAlt,FaFilter } from 'react-icons/fa'
 import './homee.css';
 
+import {APIKEYS} from '../../../../../config';
+
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: '100%',
+  height: '100%',
+  
 };
 
 const center = {
@@ -17,6 +20,7 @@ const center = {
 };
 
 export function Home() {
+  
   return (
     <div>
 
@@ -32,20 +36,24 @@ export function Home() {
       <section>
         <div className='content content-1'>
           <div className='title'><h1>Map</h1></div>
-          <img className={styles['container']} src="https://firebasestorage.googleapis.com/v0/b/charityspotdemo1.appspot.com/o/g1.png?alt=media&token=888e1913-ec8e-4508-b70a-69b590a27587"></img>
+          {/*<img className={styles['container']} src="https://firebasestorage.googleapis.com/v0/b/charityspotdemo1.appspot.com/o/g1.png?alt=media&token=888e1913-ec8e-4508-b70a-69b590a27587"></img>*/}
 
           {/*Google API Stuff */}
+          <div className={styles['container']}>
 
-          <LoadScript googleMapsApiKey="AIzaSyCiXvhZdz4T1TStQ-_3vRXUXMl0Hepe6gM">
-            <GoogleMap
-              mapContainerStyle={containerStyle}
-              center={center}
-              zoom={10}
-            >
-            </GoogleMap>
-          </LoadScript>
+            <LoadScript googleMapsApiKey = {APIKEYS.GoogleMapsAPIKey}>
+              <GoogleMap
+                
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+              >
+              </GoogleMap>
+            </LoadScript>
 
-          <h1>Hello</h1>
+          </div>
+
+          
         </div>
         <div className='content content-2'>
           <div className='title'><h1>Filter</h1></div>
