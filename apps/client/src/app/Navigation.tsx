bf9bde5f59d09913a9a7fff43f68f5564a8c5230
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-06-06 20:31:45 +0200

    Add dep to manage cookies

--- apps/client/src/app/Navigation.tsx
+++ apps/client/src/app/Navigation.tsx
@@ -3,17 +3,23 @@ import {Link} from 'react-router-dom'
 import {Navbar,Nav} from 'react-bootstrap'
 import CS from '../../../../libs/client/shared/assets/CS.png'
 
-class auth{
-  
-  checkIfUserLogIn(){
-
-    
-  }
-}
+import { getCookie, setCookie } from 'typescript-cookie'
 
 
 
 function Navigation() {
+
+  const checkIfUserLogIn = () => {
+    var ID = getCookie('ID');
+
+    if(ID == undefined){
+      console.log("User not logged in");
+    }
+
+    console.log(ID);
+
+    return true;
+  }
 
   return (
     <div>
@@ -25,7 +31,7 @@ function Navigation() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav style={{ backgroundColor: '#dcdfe3', }}className="me-auto">      
-          <Nav.Link as={Link} to={"/home"}>Home</Nav.Link>
+        { checkIfUserLogIn()  && <Nav.Link as={Link} to={"/home"}>Home</Nav.Link> }
           <Nav.Link as={Link} to={"/login"}>Login</Nav.Link>
           <Nav.Link as={Link} to={"/profile"}>Profile</Nav.Link>
           <Nav.Link as={Link} to={"/donate"}>Donate</Nav.Link>
