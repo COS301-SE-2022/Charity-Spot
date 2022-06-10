5b9df518c3c27cc0e27669fa03f8412c096b253d
lindzik <94564461+lindzik@users.noreply.github.com>
2022-06-10 06:30:11 +0200

    Update app.spec.ts

--- apps/client-e2e/src/integration/app.spec.ts
+++ apps/client-e2e/src/integration/app.spec.ts
@@ -1,78 +1,17 @@
-/*describe('client', () => {
+describe('client', () => {
 
-    //login page
-    describe('login page testing', () => {
-        beforeEach(() =>cy.visit('/login-page'));
+    describe('registration testing', () => {
+        beforeEach(() =>cy.visit('http://localhost:4200/register'));
         it('should direct to the right url', () => {
-            cy.url().should('include','login');
+            cy.url().should('include','register');
         })
+      
+      
+ /*cy.get('id') - by tag
+cy.get('.id') - by class 
+cy.get('#id')- by id*/
 
-    it('The user should be able to login', () =>{
-        //cy.visit('http://localhost:4200/login')
 
-        cy.get('input[type=email]').type("timo@email");
-        cy.get('input[type=password]').type("1234");
 
-        cy.contains('Log in').click();
-
-    })
 
 });
-
-   //registration page
-    describe('registration testing', () => {
-        beforeEach(() =>cy.visit('/registration-page'));
-        it('should direct to the right url', () => {
-            cy.url().should('include','registration');
-        })
-
-        it('The user should be able to Register', () =>{
-            //cy.visit('http://localhost:4200/register')
-            cy.contains('Charity-Spot')
-            
-            cy.get('#rgorgnm2').type("Organisation 1")
-            cy.get('input[type=email]').type("timo@email");
-            cy.get('#lct1').type("1166 Burnett Street,Hatfield");
-            cy.get('#rgpwd1').type("1234");
-            cy.get('#rgpwd2').type("1234");
-            
-    
-            cy.contains('Register').click();
-            cy.contains('click to Login').click();
-        })
-
-    });
-
-      //organised profile page
-    describe('organised profile page testing', () => {
-        beforeEach(() =>cy.visit('/organised-profile-page'));
-        it('should direct to the right url', () => {
-            cy.url().should('include','organised');
-        })
-
-    it('The user should be able to view their organisations profile page', () =>{
-        //cy.visit('http://localhost:4200/organised-profile')
-
-        cy.contains('#profile-pic');
-
-        cy.contains('#logout1');
-        cy.contains('Type');
-        cy.contains('Email');
-        cy.contains('Name');
-        cy.contains('Date Registered');
-        cy.contains('Location');
-
-        cy.contains('Home').click();
-
-
-/*cy.get('id') - by tag
-cy.get('.id') - by class 
-cy.get('#id')- by id
-
-    })
-    
-    });
-
-
-
-});*/
