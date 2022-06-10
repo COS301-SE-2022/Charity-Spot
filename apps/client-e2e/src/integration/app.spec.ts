078c3c822250ff2ccc9e563e01fb2e54fcb5e1ac
lindzik <94564461+lindzik@users.noreply.github.com>
2022-06-10 06:31:05 +0200

    Update app.spec.ts

--- apps/client-e2e/src/integration/app.spec.ts
+++ apps/client-e2e/src/integration/app.spec.ts
@@ -6,6 +6,14 @@ describe('client', () => {
             cy.url().should('include','register');
         })
       
+      it('The user should be able to Register', () =>{
+           cy.visit('http://localhost:4200/register')
+            
+            cy.get('#rgorgnm1').select('Organization').should('have.value','Organization');
+
+            cy.get('#rgorgnm2').type("Organisation 1")
+            cy.get('input[type=email]').type("test@gmail.com");
+         })
       
  /*cy.get('id') - by tag
 cy.get('.id') - by class 
