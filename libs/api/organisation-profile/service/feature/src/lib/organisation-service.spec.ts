9a48c73139b7e582686a3820d8ca5b1d8b951dbf
lindzik <94564461+lindzik@users.noreply.github.com>
2022-06-10 06:45:16 +0200

    Create organisation-service.spec.ts

---
+++ libs/api/organisation-profile/service/feature/src/lib/organisation-service.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { OrganisationService } from './organisation.service';
+import { OrganisationEntity } from './organisation.entity';
+
+import { OrganisationRepository } from '@charity-spot/api/organisation-profile/repository/data-access';
+import { PrismaService } from '@charity-spot/api/shared/services/prisma';
+
+
+
+const organisationEntity = new OrganisationEntity();
+
+describe ( 'Organisation Service', () => {
+    let service: OrganisationService;
+    let repository: OrganisationRepository;
+    let prisma: PrismaService;
+    let entity: OrganisationEntity;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+          providers: [OrganisationService, OrganisationRepository, PrismaService, OrganisationEntity],
+        }).compile();
+
+        service = module.get<OrganisationService>(OrganisationService);
+
+  });
