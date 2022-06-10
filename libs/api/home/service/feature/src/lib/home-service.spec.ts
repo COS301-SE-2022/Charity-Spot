74590a4fb623ea7d04b73a4b7cbc5bd8f043b531
lindzik <94564461+lindzik@users.noreply.github.com>
2022-06-10 06:41:43 +0200

    Create home-service.spec.ts

---
+++ libs/api/home/service/feature/src/lib/home-service.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { HomeService } from './home.service';
+import { HomeEntity } from './home.entity';
+
+import { HomeRepository } from '@charity-spot/api/home/repository/data-access';
+import { PrismaService } from '@charity-spot/api/shared/services/prisma';
+
+
+
+const homeEntity = new HomeEntity();
+
+describe ( 'Home Service', () => {
+    let service: HomeService;
+    let repository: HomeRepository;
+    let prisma: PrismaService;
+    let entity: HomeEntity;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+          providers: [HomeService, HomeRepository, PrismaService, HomeEntity],
+        }).compile();
+
+        service = module.get<HomeService>(HomeService);
+
+  });
