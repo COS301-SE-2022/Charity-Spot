4a58951f56a3abd36c0d9dfef72c6ba0c5022b68
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-07-05 12:06:11 +0200

    Created UI files for chat feature

---
+++ libs/client/chat/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'client-chat',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/client/chat',
+};
