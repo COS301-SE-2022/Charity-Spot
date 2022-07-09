0f041e44f558977cd636a32c270ffc4ea6e327b2
nimGaluu <u20451696@tuks.co.za>
2022-07-09 18:30:59 +0200

    🚧 (Service): Message retrieval logic

--- libs/api/chat/service/feature/src/lib/chat.service.ts
+++ libs/api/chat/service/feature/src/lib/chat.service.ts
@@ -10,24 +10,31 @@ export class ChatService {
         //retrieve file from database 
 
         let messages = "AG0AZQBzAHMAYQBnAGUAcwBfAGkAbgBfAGIAYQBzAGUANgA0";
-
-        messages = Buffer.from(messages, 'base64').toString('utf-8');
-
-        messages = message + message;
-
-        message = Buffer.from(messages, 'utf-8').toString('base64');
+            messages = Buffer.from(messages, 'base64').toString('utf-8');
+            messages = message + message;
+            message = Buffer.from(messages, 'utf-8').toString('base64');
 
         //update the file in database
 
         const returnableV = new ChatEntity();
-        returnableV.Reciever = to;
-        returnableV.Sender = from;
-        returnableV.Message = message;
+            returnableV.Reciever = to;
+            returnableV.Sender = from;
+            returnableV.Message = message;
 
         return returnableV;
     }
 
     async RetrieveMessages(userID: string) {
-        return null;
+        // retrieve file from database
+
+        let messages = "AG0AZQBzAHMAYQBnAGUAcwBfAGkAbgBfAGIAYQBzAGUANgA0";
+            messages = Buffer.from(messages, 'base64').toString('utf-8');
+
+        const returnableV = new ChatEntity();
+            returnableV.Reciever = "";
+            returnableV.Sender = userID;
+            returnableV.Message = messages;
+
+        return returnableV;
     }
 }
