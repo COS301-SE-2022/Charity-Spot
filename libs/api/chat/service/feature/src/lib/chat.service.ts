b7a99b3ab4f673f13f7566e9ad69d47d37777a40
nimGaluu <u20451696@tuks.co.za>
2022-07-09 18:14:51 +0200

    🚧 (Service) : "Send" service

--- libs/api/chat/service/feature/src/lib/chat.service.ts
+++ libs/api/chat/service/feature/src/lib/chat.service.ts
@@ -6,14 +6,25 @@ import { ChatRepository } from '@charity-spot/api/chat/repository/feature'
 export class ChatService {
     constructor(private ChatRepository: ChatRepository) {}
 
-    async TestFunc() {
-        const entity = new ChatEntity();
-        entity.ID = "Chat is working!";
-        return entity;
-    }
-
     async Send(to: string, from: string, message: string) {
-        return null;
+        //retrieve file from database 
+
+        let messages = "AG0AZQBzAHMAYQBnAGUAcwBfAGkAbgBfAGIAYQBzAGUANgA0";
+
+        messages = Buffer.from(messages, 'base64').toString('utf-8');
+
+        messages = message + message;
+
+        message = Buffer.from(messages, 'utf-8').toString('base64');
+
+        //update the file in database
+
+        const returnableV = new ChatEntity();
+        returnableV.Reciever = to;
+        returnableV.Sender = from;
+        returnableV.Message = message;
+
+        return returnableV;
     }
 
     async RetrieveMessages(userID: string) {
