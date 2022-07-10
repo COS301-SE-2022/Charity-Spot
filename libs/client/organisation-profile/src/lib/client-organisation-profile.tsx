da31a701605e6bb4ffb6984634344d55c2d2daa6
Moloko <molomag.mm@gmail.com>
2022-07-10 15:55:22 +0200

    Profile Update UI

--- libs/client/organisation-profile/src/lib/client-organisation-profile.tsx
+++ libs/client/organisation-profile/src/lib/client-organisation-profile.tsx
@@ -197,19 +197,23 @@ export function Profile() {
               <div className="cover">Date Registered: { ODate }</div>
               <div className="cover">Location: { OLocation}</div>
               <div className="cover">Rating: 6.0</div>
-      
-              <div className="rate">
-                <input type="radio" id="star5" name="rate" value="5" />
-                <label htmlFor="star5" title="text"></label>
-                <input type="radio" id="star4" name="rate" value="4" />
-                <label htmlFor="star4" title="text"></label>
-                <input type="radio" id="star3" name="rate" value="3" />
-                <label  htmlFor="star3" title="text"></label>
-                <input type="radio" id="star2" name="rate" value="2" />
-                <label  htmlFor="star2" title="text"></label>
-                <input type="radio" id="star1" name="rate" value="1" />
-                <label  htmlFor="star1" title="text"></label>
+
+              <div className="cover2"> 
+                  <p>Rate Organization</p>
+                  <div className="rate"> 
+                    <input type="radio" id="star5" name="rate" value="5" />
+                    <label htmlFor="star5" title="text"></label>
+                    <input type="radio" id="star4" name="rate" value="4" />
+                    <label htmlFor="star4" title="text"></label>
+                    <input type="radio" id="star3" name="rate" value="3" />
+                    <label  htmlFor="star3" title="text"></label>
+                    <input type="radio" id="star2" name="rate" value="2" />
+                    <label  htmlFor="star2" title="text"></label>
+                    <input type="radio" id="star1" name="rate" value="1" />
+                    <label  htmlFor="star1" title="text"></label>
+                  </div>
               </div>
+              
           </div>
       </div>
    </div> 
