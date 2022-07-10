f4534e28708686dfc075649d2b9d9a9eb3453e07
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-07-10 15:44:10 +0200

    Working on NN class

--- tools/NeuralNetwork/NeuralNetwork.java
+++ tools/NeuralNetwork/NeuralNetwork.java
@@ -1,1 +1,13 @@
-public class NeuralNetwork{}
+public class NeuralNetwork{
+    //1. Weight matrix for the input and hidden layer
+    //2. Weight matrix for the hidden and output layer
+    //3. Bias matrix for the hidden layer
+    //4. Bias matrix for the ouput layer
+    //5. Learing rate
+
+    Matrix input_hidden_weights;
+    Matrix hidden_output_weights;
+    Matrix bias_for_hidden;
+    Matrix bias_for_output;
+    double learning_rate = 0.01;
+}
