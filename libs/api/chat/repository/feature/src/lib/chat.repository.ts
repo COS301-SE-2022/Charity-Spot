425c860f2cb6ccf17a25acb794a9767827259389
David Roodt <davidroodt2@gmail.com>
2022-07-11 08:01:41 +0200

    addMessages Repo

--- libs/api/chat/repository/feature/src/lib/chat.repository.ts
+++ libs/api/chat/repository/feature/src/lib/chat.repository.ts
@@ -5,4 +5,17 @@ import { PrismaService } from '@charity-spot/api/shared/services/prisma';
 export class ChatRepository {
   constructor(private prisma: PrismaService) {}
 
+  async AddMessages(orgID, clientID : string ,text: string)
+  {
+    const u = await this.prisma.chatHistory.create({
+      data:
+      {
+        OrgID: orgID,
+        ClientID:clientID,
+        Messages:text
+      }
+    })
+
+    return u;
+  }
 }
