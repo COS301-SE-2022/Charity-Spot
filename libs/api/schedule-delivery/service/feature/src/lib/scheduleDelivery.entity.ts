9b3ef76b9776824db07ce8cd1672d1d1c6252f0f
nimGaluu <u20451696@tuks.co.za>
2022-07-16 18:07:53 +0200

    🚧 (API + SERVICE): Making a schedule for item

--- libs/api/schedule-delivery/service/feature/src/lib/scheduleDelivery.entity.ts
+++ libs/api/schedule-delivery/service/feature/src/lib/scheduleDelivery.entity.ts
@@ -2,6 +2,12 @@ import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class ScheduleDeliveryEntity {
-    @Field({ nullable: true })
-    ID: string
+    @Field({ nullable: false })
+    id_1: string
+
+    @Field({nullable: false})
+    id_2: string
+
+    @Field({nullable: false})
+    id_item: string
 }
