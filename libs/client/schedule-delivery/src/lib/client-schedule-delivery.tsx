ddd4787201a047db9aa22841b07c7e0030c49700
Moloko <molomag.mm@gmail.com>
2022-07-17 01:24:10 +0200

    Done with the other delivery Tab

--- libs/client/schedule-delivery/src/lib/client-schedule-delivery.tsx
+++ libs/client/schedule-delivery/src/lib/client-schedule-delivery.tsx
@@ -12,7 +12,7 @@ export function ClientScheduleDelivery(props: ClientScheduleDeliveryProps) {
   <input type ="radio" name="slider" id='history1' ></input>
   
   <nav>
-    <label htmlFor= "deliver1" className='deliver1'><FaClock/> Delievery </label>
+    <label htmlFor= "deliver1" className='deliver1'><FaClock/> Delivery </label>
     <label htmlFor= "history1" className='history1'> <FaHistory/> History </label>
     <div className='slider'></div>
   </nav>
@@ -25,38 +25,38 @@ export function ClientScheduleDelivery(props: ClientScheduleDeliveryProps) {
                 <div className='delvrr'>
                   <form id = "mainDeliverForm">
                     <div className='deliver-box'>
-                      <label>From:</label><br/>
+                      <label className='labelDel'>From:</label><br/>
                       <input className="del1" type ="text" placeholder='Your Name'></input>  
                         <FaPen color='#1458b3'/><br/>
                     </div>
                     
                     <div className='deliver-box'>
                       
-                        <label>To:</label><br/>
+                        <label className='labelDel'>To:</label><br/>
                         <input className="del1" type ="text" placeholder='Recipient Name'></input>  
                         <FaPen color='#1458b3'/>
                     </div>  
                     <div className='deliver-box'>
                       
-                      <label>Your Location:</label><br/>
+                      <label className='labelDel'>Your Location:</label><br/>
                       <input className="del1" type ="text" placeholder=' e.g Pretoria, Hatfield'></input>  
                       <FaPen color='#1458b3'/>
                   </div>  
                   <div className='deliver-box'>
                       
-                      <label>Delivery Location:</label><br/>
+                      <label className='labelDel'>Delivery Location:</label><br/>
                       <input className="del1" type ="text" placeholder=' Limpopo, Polokwane'></input>  
                       <FaPen color='#1458b3'/>
                   </div>  
                   <div className='deliver-box'>
                       
-                      <label>Date of Transportation:</label><br/>
+                      <label className='labelDel'>Date of Transportation:</label><br/>
                       <input className="del1" type ="date" ></input>  
                       <FaPen color='#1458b3'/>
                   </div>  
                   <div className='deliver-box'>
                       
-                      <label>Estimated Date of Arrival:</label><br/>
+                      <label className='labelDel'>Estimated Date of Arrival:</label><br/>
                       <input className="del1" type ="date" ></input>  
                       <FaPen color='#1458b3'/>
                   </div>  
@@ -77,7 +77,34 @@ export function ClientScheduleDelivery(props: ClientScheduleDeliveryProps) {
     <div className='content content-2'>
            <div className='title'><h1>History</h1></div>
 
-             
+              <div className='rapper'>
+
+                <div className='collapsible'>
+
+                    <input type ='checkbox' id = "deee1"></input>
+
+                    <label htmlFor="deee1">Jacket </label>
+
+                    <div className='collapsible-textDel'><br/>
+                        <div className='collapseleftDel'>
+                        <img src="" alt="" id="donation-pic2"/>
+                        </div>
+
+                        <div className='collapserightDel'>
+      
+                            <div className="cov">Item Name: Jacket </div>
+                            <div className="cov">Name of Charity: Capricorn Highschool </div>
+                            <div className="cov">From: Pretoria Hatfield</div> 
+                            <div className="cov">To: Limpopo Polokwane </div> 
+                            <div className="cov">Date Delivered: 17/07/2022 </div> 
+                            <div className="covDesc">Description: </div>
+                        </div>
+                        
+                    </div>
+
+                </div>
+
+            </div>           
                 
   </div>
   </section>
