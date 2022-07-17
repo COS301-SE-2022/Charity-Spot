2c85c2e48b1f680fb50d9b49e88543cadb1abf50
David Roodt <davidroodt2@gmail.com>
2022-07-17 09:46:04 +0200

    GetListOfOrgs

--- libs/api/schedule-delivery/repository/feature/src/lib/scheduleDelivery.repository.ts
+++ libs/api/schedule-delivery/repository/feature/src/lib/scheduleDelivery.repository.ts
@@ -5,4 +5,21 @@ import { PrismaService } from '@charity-spot/api/shared/services/prisma';
 export class ScheduleDeliveryRepository {
   constructor(private prisma: PrismaService) {}
 
+  //Get All Orgs with the desired Item
+
+  async GetListOfOrgsWithItem(itemName:string){
+    const u = await this.prisma.donoItem.findMany({
+      select:
+      {
+        ItemID : true,
+        OrgID : true
+      },
+      where:
+      {
+        ItemName: itemName
+      }
+    });
+
+    return u;
+  }
 }
