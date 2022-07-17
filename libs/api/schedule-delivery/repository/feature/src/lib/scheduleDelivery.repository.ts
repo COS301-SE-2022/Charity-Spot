7676bcb66ca349d6a49c155f01e137714c655c4b
David Roodt <davidroodt2@gmail.com>
2022-07-17 10:05:53 +0200

    Find All Org Del

--- libs/api/schedule-delivery/repository/feature/src/lib/scheduleDelivery.repository.ts
+++ libs/api/schedule-delivery/repository/feature/src/lib/scheduleDelivery.repository.ts
@@ -28,4 +28,25 @@ export class ScheduleDeliveryRepository {
   async CreateShedule(itemID:string, loaction: string, Time: string, ClientID: string){
     //To Be Added
   }
+
+  //Schedule Delivery
+
+  async FindAllOrgDeliviries(OrgID:string){
+    const u = await this.prisma.donoItem.findMany({
+      select:
+      {
+        ItemID : true
+      },
+      where:
+      {
+        OrgID: OrgID
+      }
+    });
+
+    let result;
+
+    u.forEach(item => {
+      result.push(/*to add getting result from delivery table*/);
+    });
+  }
 }
