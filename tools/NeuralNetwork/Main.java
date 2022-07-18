829fd34b0b6e0db009c3f98165f2e34c28fdbf8f
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-07-18 22:23:24 +0200

    AI seems to be working, still need to refine

--- tools/NeuralNetwork/Main.java
+++ tools/NeuralNetwork/Main.java
@@ -15,9 +15,33 @@ public class Main {
 			
 			String line;
 
+			int j = 0;
+
 			while((line = br.readLine()) != null){
 
+				trainData[j] = new double[3];
+				correctOutput[j] = new double[4];
+				int k = 0;
+
 				for (int i=0; i < line.length(  ); i++){
+
+					if(i==0){
+
+						if(line.charAt(i) == '1'){
+							correctOutput[j] = new double[]{1, 0, 0, 0};
+						}
+						else if(line.charAt(i) == '2'){
+							correctOutput[j] = new double[]{0, 1, 0, 0};
+						}
+						else if(line.charAt(i) == '3'){
+							correctOutput[j] = new double[]{0, 0, 1, 0};
+						}
+						else if(line.charAt(i) == '4'){
+							correctOutput[j] = new double[]{0, 0, 0, 1};
+						}
+
+						continue;
+					}
 
 					if(line.charAt(i) == ' ' || line.charAt(i) == ','){
 						continue;
@@ -25,42 +49,62 @@ public class Main {
 
 					double val = (double) (line.charAt(i) - '0');
 
-    				System.out.print(val);
+					trainData[j][k] = val;
+
+    				k++;
 
 				}
 
-				System.out.println();
+				j++;
 
-			}  
-
-			
+			}
 
 		}
 		catch(Exception e){}
 
-		System.out.println(trainData.length);
+		for(int i=0; i<trainData.length; i++){
+
+			for(int jj=0; jj<trainData[i].length; jj++){
+
+				System.out.print(trainData[i][jj] + ", ");
+
+			}
+
+			System.out.println();
+
+		}
+
+		for(int i=0; i<correctOutput.length; i++){
+
+			for(int jj=0; jj<correctOutput[i].length; jj++){
+
+				System.out.print(correctOutput[i][jj] + ", ");
+
+			}
+
+			System.out.println();
+
+		}    
+
+	
 
 	
 		
-		/*NeuralNetwork nn = new NeuralNetwork(3,10,4);
-
-		/* [
+		NeuralNetwork nn = new NeuralNetwork(3,15,4);
 		
 		
 		List<Double>output;
 		
-		nn.fit(trainData, correctOutput, 50000);
+		nn.fit(trainData, correctOutput, 60000);
 
 
-		double [][] input = {
-				{0,0},{0,1},{1,0},{1,1}	
-		};
+		
 
-		for(double d[]:input)
+		for(double d[]:trainData)
 		{
 			output = nn.predict(d);
 			System.out.println(output.toString());
-		}*/		
+		}		
 
 	}
 
