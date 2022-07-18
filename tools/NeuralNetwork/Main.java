e6f682a638291a6b57edeae6f79d0cdd388f70e2
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-07-18 21:44:59 +0200

    Working on AI

--- tools/NeuralNetwork/Main.java
+++ tools/NeuralNetwork/Main.java
@@ -1,29 +1,55 @@
 import java.util.List;
+import java.io.*;
 
 public class Main {
 
-    static double [][] trainData= {
-			{0,0},
-			{1,0},
-			{0,1},
-			{1,1}
-	};
-	static double [][] correctOutput= {
-			{0},{1},{1},{0}
-	};
+    static double [][] trainData= new double[360][];
+
+	static double [][] correctOutput= new double[360][];
 
 	public static void main(String[] args) {
-		
-		NeuralNetwork nn = new NeuralNetwork(2,10,1);
 
-		/* [*/
+		try{
+
+			PushbackInputStream input = new PushbackInputStream(new FileInputStream("mockDonations.txt"));
+
+			int charV = input.read();
+
+			String st;
+
+			int i = 0;
+
+			while (charV != -1){
+
+				trainData[i] = new double[3];
+				trainData[i][0] = 1; 
+
+				if(charV == 10){
+					System.out.print(" new line");
+				}
+
+				System.out.print((char)charV);
+
+				charV = input.read();
+			}
+
+		}
+		catch(Exception e){}
+
+		System.out.println(trainData[0].length);
+
+	
+		
+		/*NeuralNetwork nn = new NeuralNetwork(2,10,1);
+
+		/* [
 		
 		
 		List<Double>output;
 		
 		nn.fit(trainData, correctOutput, 50000);
 
-		
+
 		double [][] input = {
 				{0,0},{0,1},{1,0},{1,1}	
 		};
@@ -32,7 +58,7 @@ public class Main {
 		{
 			output = nn.predict(d);
 			System.out.println(output.toString());
-		}		
+		}*/		
 
 	}
 
