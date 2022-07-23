0e10687958a723ff25a9800b1522740196d08285
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-07-23 21:25:56 +0200

    Added framework files for itemRequestResults

---
+++ libs/client/item-request-results/src/lib/client-item-request-results.tsx
@@ -0,0 +1,14 @@
+import styles from './client-item-request-results.module.css';
+
+/* eslint-disable-next-line */
+export interface ClientItemRequestResultsProps {}
+
+export function ClientItemRequestResults(props: ClientItemRequestResultsProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to ClientItemRequestResults!</h1>
+    </div>
+  );
+}
+
+export default ClientItemRequestResults;
