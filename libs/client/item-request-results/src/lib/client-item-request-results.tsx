2dde2fbd6e8c81b41f037f7110dd7a98f899970b
Moloko <molomag.mm@gmail.com>
2022-07-23 23:49:54 +0200

    Suggested Orgs done

--- libs/client/item-request-results/src/lib/client-item-request-results.tsx
+++ libs/client/item-request-results/src/lib/client-item-request-results.tsx
@@ -6,25 +6,104 @@ export function ClientItemRequestResults(props: ClientItemRequestResultsProps) {
   return (
     <div className='motherHolder'>
       <br/>
-      <h2>Suggested Organizations</h2>
+      <h2 className='rqq'>Suggested Organizations</h2>
       <div className='HoldAll'>
         <div className='leftHolda'>
-        <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" id="profile-pic"></img>
-          <div className='Lead'><h4>Seal Organization</h4></div>
-          <div className='within'>
-            <p>We offer high quality food, and we are the best in the business</p>
-          </div>
-          <button type='submit' id='chatGo'>Choose</button>
-          
+
+            <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" id="profile-pic"></img>
+              <div className='Lead'><h4>Seal Organization</h4></div>
+              <div className='within'>
+                <p>We offer high quality food, and we are the best in the business</p>
+              </div>
+
+                    <div className="ratedsmalla"> 
+                          <input type="radio" id="starreq1" checked = {true} name="req0" value="5" disabled />
+                          <label htmlFor="starreq1" title="text"></label>
+                          <input type="radio" id="starreq2"   name="req0" value="4"  disabled/>
+                          <label htmlFor="starreq2"title="text"></label>
+                          <input type="radio" id="starreq3" name="req0" value="3"  disabled />
+                          <label  htmlFor="starreq3" title="text"></label>
+                          <input type="radio" id="starreq4" name="req0" value="2"  disabled/>
+                          <label  htmlFor="starreq4" title="text"></label>
+                          <input type="radio" id="starreq5" name="req0" value="1"  disabled/>
+                          <label  htmlFor="starreq6" title="text"></label>
+                    </div> 
+                    <br></br>   
+              <button type='submit' id='butChoose'>Choose</button> 
+
         </div>
+
         <div className='middleHolda'>
-          <h3>Company2</h3>
+
+        <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" id="profile-pic"></img>
+              <div className='Lead'><h4>Whole Foods</h4></div>
+              <div className='within'>
+                <p>Organic and Highquality food, been in busniess since 1987</p>
+              </div>
+
+                    <div className="ratedsmalla"> 
+                          <input type="radio" id="starreq1"  name="req1" value="5" disabled />
+                          <label htmlFor="starreq1" title="text"></label>
+                          <input type="radio" id="starreq2" checked = {true}   name="req01" value="4"  disabled/>
+                          <label htmlFor="starreq2"title="text"></label>
+                          <input type="radio" id="starreq3" name="req1" value="3"  disabled />
+                          <label  htmlFor="starreq3" title="text"></label>
+                          <input type="radio" id="starreq4" name="req1" value="2"  disabled/>
+                          <label  htmlFor="starreq4" title="text"></label>
+                          <input type="radio" id="starreq5" name="req1" value="1"  disabled/>
+                          <label  htmlFor="starreq6" title="text"></label>
+                    </div> 
+                    <br></br>   
+              <button type='submit' id='butChoose'>Choose</button> 
+
         </div>
         <div className='righttHolda'>
-          <h3>Company3</h3>
+
+        <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" id="profile-pic"></img>
+              <div className='Lead'><h4>Joes Meat and Pap</h4></div>
+              <div className='within'>
+                <p>Lekker Braai specials for the whole family to enjoy</p>
+              </div>
+
+                    <div className="ratedsmalla"> 
+                          <input type="radio" id="starreq1"  name="req2" value="5" disabled />
+                          <label htmlFor="starreq1" title="text"></label>
+                          <input type="radio" id="starreq2"  name="req2" value="4"  disabled/>
+                          <label htmlFor="starreq2"title="text"></label>
+                          <input type="radio" id="starreq3" name="req2"  checked = {true} value="3"  disabled />
+                          <label  htmlFor="starreq3" title="text"></label>
+                          <input type="radio" id="starreq4" name="req2" value="2"  disabled/>
+                          <label  htmlFor="starreq4" title="text"></label>
+                          <input type="radio" id="starreq5" name="req2" value="1"  disabled/>
+                          <label  htmlFor="starreq6" title="text"></label>
+                    </div> 
+                    <br></br>   
+              <button type='submit' id='butChoose'>Choose</button> 
+
         </div>
         <div className='leftHolda'>
-          <h3>Company3</h3>
+
+        <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" id="profile-pic"></img>
+              <div className='Lead'><h4>Farmed Greens</h4></div>
+              <div className='within'>
+                <p>We primarily focus on Green Veggies.</p>
+              </div>
+
+                    <div className="ratedsmalla"> 
+                          <input type="radio" id="starreq1"  name="req3" value="5" disabled />
+                          <label htmlFor="starreq1" title="text"></label>
+                          <input type="radio" id="starreq2"  name="req3" value="4"  disabled/>
+                          <label htmlFor="starreq2"title="text"></label>
+                          <input type="radio" id="starreq3" name="req3"   value="3"  disabled />
+                          <label  htmlFor="starreq3" title="text"></label>
+                          <input type="radio" id="starreq4" name="req3" value="2"  disabled/>
+                          <label  htmlFor="starreq4" title="text"></label>
+                          <input type="radio" id="starreq5" name="req3" checked = {true} value="1"  disabled/>
+                          <label  htmlFor="starreq6" title="text"></label>
+                    </div> 
+                    <br></br>   
+              <button type='submit' id='butChoose'>Choose</button> 
+
         </div>
         
         
