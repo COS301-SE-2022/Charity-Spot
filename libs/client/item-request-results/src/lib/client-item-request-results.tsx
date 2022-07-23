ee3893ff0fcc2332c9ff44ce7d7e0268fc41d849
Moloko <molomag.mm@gmail.com>
2022-07-24 00:12:33 +0200

    Sprint Complete Modified Image Resolution

--- libs/client/item-request-results/src/lib/client-item-request-results.tsx
+++ libs/client/item-request-results/src/lib/client-item-request-results.tsx
@@ -10,7 +10,7 @@ export function ClientItemRequestResults(props: ClientItemRequestResultsProps) {
       <div className='HoldAll'>
         <div className='leftHolda'>
 
-            <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" id="profile-pic"></img>
+            <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" className="orgreqpic"></img>
               <div className='Lead'><h4>Seal Organization</h4></div>
               <div className='within'>
                 <p>We offer high quality food, and we are the best in the business</p>
@@ -35,7 +35,7 @@ export function ClientItemRequestResults(props: ClientItemRequestResultsProps) {
 
         <div className='middleHolda'>
 
-        <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" id="profile-pic"></img>
+        <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" className="orgreqpic"></img>
               <div className='Lead'><h4>Whole Foods</h4></div>
               <div className='within'>
                 <p>Organic and Highquality food, been in busniess since 1987</p>
@@ -59,7 +59,7 @@ export function ClientItemRequestResults(props: ClientItemRequestResultsProps) {
         </div>
         <div className='righttHolda'>
 
-        <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" id="profile-pic"></img>
+        <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" className="orgreqpic"></img>
               <div className='Lead'><h4>Joes Meat and Pap</h4></div>
               <div className='within'>
                 <p>Lekker Braai specials for the whole family to enjoy</p>
@@ -83,7 +83,7 @@ export function ClientItemRequestResults(props: ClientItemRequestResultsProps) {
         </div>
         <div className='leftHolda'>
 
-        <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" id="profile-pic"></img>
+        <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" className="orgreqpic"></img>
               <div className='Lead'><h4>Farmed Greens</h4></div>
               <div className='within'>
                 <p>We primarily focus on Green Veggies.</p>
