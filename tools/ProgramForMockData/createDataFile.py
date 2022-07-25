192b067e4177ce11438cf38d7bcd59b0233dc3d4
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-07-25 10:00:44 +0200

    Still working on program to create data file for AI

--- tools/ProgramForMockData/createDataFile.py
+++ tools/ProgramForMockData/createDataFile.py
@@ -1,6 +1,36 @@
 import psycopg2
 import random
 
+import datetime
+import calendar
+
+def findDay(date):
+    day, month, year = (int(i) for i in date.split(','))   
+    dayNumber = calendar.weekday(year, month, day)
+    return dayNumber+1
+    #givenDate = datetime.datetime.strptime(date, '%d,%m,%Y').weekday()
+    #return (calendar.day_name[born])
+
+def findLoc(loc):
+
+    locations = ["Pretoria", "Johannesburg", "Cape Town", "Bloemfontein", "Polokwane", "Durban"]
+
+    for i in range(0, len(locations)):
+        if(loc == locations[i]):
+            return i+1
+
+    return 0
+
+def findItemType(item):
+
+    locations = ["Pretoria", "Johannesburg", "Cape Town", "Bloemfontein", "Polokwane", "Durban"]
+
+    for i in range(0, len(locations)):
+        if(loc == locations[i]):
+            return i+1
+
+    return 0
+
 try:
     connection = psycopg2.connect(user="seal_team",
                                   password="seal_team",
@@ -17,7 +47,14 @@ try:
     row = cursor.fetchone()
 
     while row is not None:
-        print(row)
+
+        orgID = row[0] 
+        date = row[1]
+        location = str(findLoc(row[2]))
+        item_type = row[3]
+
+        print(orgID + " " + date + " " + location + " " + item_type)
+
         row = cursor.fetchone()
 
 except (Exception, psycopg2.Error) as error:
@@ -29,3 +66,7 @@ finally:
         cursor.close()
         connection.close()
         print("PostgreSQL connection is closed")
+ 
+# Driver program
+date = '28,07,2022'
+print(findDay(date))
