657afbec4f71f62a8c335105b116bece3e7cc490
lindzik <94564461+lindzik@users.noreply.github.com>
2022-07-28 18:54:15 +0200

    Update chat-service.spec.ts

--- libs/api/chat/service/feature/src/lib/chat-service.spec.ts
+++ libs/api/chat/service/feature/src/lib/chat-service.spec.ts
@@ -8,3 +8,24 @@ import { ChatEntity } from './chat.entity';
 import { ChatRepository } from '@charity-spot/api/chat/repository/feature'
 import { PrismaService } from '@charity-spot/api/shared/services/prisma';
 import { async } from '@firebase/util';
+
+
+const chatEntity = new ChatEntity();
+
+describe ( 'Chat Service', () => {
+    let service: ChatService;
+    let repository: ChatRepository;
+    let prisma: PrismaService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+          providers: [ChatService, ChatRepository,ChatEntity, PrismaService],
+        }).compile();
+
+        service = module.get<ChatService>(ChatService);
+
+  });
+  it('Chat service should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
