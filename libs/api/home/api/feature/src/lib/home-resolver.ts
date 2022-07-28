7f1c935de1ed19b6b6152e3434953b92aa786b08
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-07-28 03:50:12 +0200

    Still need to finish map

--- libs/api/home/api/feature/src/lib/home-resolver.ts
+++ libs/api/home/api/feature/src/lib/home-resolver.ts
@@ -11,4 +11,11 @@ export class HomeResolver {
         
         return this.HomeService.getAllOrg();
     }
+
+    @Query(() => [HomeEntity])
+    GetAllItems(){
+
+        return this.HomeService.getAllItems();
+
+    }
 }
