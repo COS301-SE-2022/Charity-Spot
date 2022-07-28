ecdb84866323ed581c9908be352cea3b24c73b4d
lindzik <94564461+lindzik@users.noreply.github.com>
2022-07-28 19:10:20 +0200

    Update organisation-service.spec.ts

--- libs/api/organisation-profile/service/feature/src/lib/organisation-service.spec.ts
+++ libs/api/organisation-profile/service/feature/src/lib/organisation-service.spec.ts
@@ -30,3 +30,42 @@ describe ( 'Organisation Service', () => {
     expect(service).toBeDefined();
   });
 });
+
+
+  let resolver: OrganisationService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        OrganisationService,
+        OrganisationService,
+        OrganisationService,
+        OrganisationEntity,
+        OrganisationRepository,
+        PrismaService,
+      ],
+    }).compile();
+    resolver = module.get<OrganisationService>(OrganisationService);
+  });
+
+  //getOrgProfile(userID: string)
+/*it('Should Match an Organisation Profile', async () => { 
+  jest
+    .spyOn(resolver,'getOrgProfile')
+    //.mockImplementation(() => Promise<OrganisationEntity>);
+    .mockImplementation((userID: string) => OrganisationEntity{
+    
+    });
+    expect(resolver.getOrgProfile).not.toHaveBeenCalled();
+    expect(await resolver.getOrgProfile("cl62huz0m0002f6ynu0z40get")).toMatchObject(organisationEntity)
+    expect(resolver.getOrgProfile).toHaveBeenCalled();
+});*/
+
+//updateDet(id: string, name: string, loc: string, picture: string, password: string)
+/*it('Should Match an Organisation Entity', async () => {
+jest
+  .spyOn(resolver,'updateDet')
+  .mockImplementation();
+  expect(resolver.updateDet).not.toHaveBeenCalled();
+  expect(await service.updateDet("cl62huz0m0002f6ynu0z40get","test","Pretoria","asdadwdadddd","123")).toMatchObject(organisationEntity);
+  expect(resolver.updateDet).toHaveBeenCalled();
+});*/
