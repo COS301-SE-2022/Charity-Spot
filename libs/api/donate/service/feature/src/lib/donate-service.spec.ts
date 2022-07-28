6951517e69acc980163e7f251d7eb4a0984a0304
lindzik <94564461+lindzik@users.noreply.github.com>
2022-07-28 18:58:27 +0200

    Update donate-service.spec.ts

--- libs/api/donate/service/feature/src/lib/donate-service.spec.ts
+++ libs/api/donate/service/feature/src/lib/donate-service.spec.ts
@@ -56,3 +56,26 @@ it('Should retireve organisation picture',async () => {
     expect(resolver.getItemPicDirec).toHaveBeenCalled();
 
 });
+
+
+//history(id: string)
+it('Should return history of a user', async () => {
+  jest
+    .spyOn(resolver,'history')
+    .mockImplementation( (): Promise<DonateEntity> => Promise.resolve(donateEntity));
+    expect(resolver.history).not.toHaveBeenCalled();
+    expect(await resolver.history("cl62huz0m0002f6ynu0z40get")).toMatchObject(donateEntity);
+    expect(resolver.history).toHaveBeenCalled();
+});
+
+//donate(id: string, name: string, quantity: number, category: catagory, condition: quality, descr: string)
+it('Should allow a user to donate an item', async () => {
+  jest
+    .spyOn(resolver,'donate')
+    .mockImplementation( (): Promise<DonateEntity> => Promise.resolve(donateEntity));
+    expect(resolver.donate).not.toHaveBeenCalled();
+    expect(await resolver.donate("cl62huz0m0002f6ynu0z40get","Jeans",2,catagory.CLOTHING,quality.USED,"This is a pair of old jeans.")).toMatchObject(donateEntity);
+    expect(resolver.donate).toHaveBeenCalled();
+});
+
+// PART 1 - END
