33cefe1e159f17acf39398c339d4c195e880e4dc
lindzik <94564461+lindzik@users.noreply.github.com>
2022-07-28 19:07:03 +0200

    Update login-service.spec.ts

--- libs/api/login/service/feature/src/lib/login-service.spec.ts
+++ libs/api/login/service/feature/src/lib/login-service.spec.ts
@@ -26,41 +26,4 @@ describe ( 'LoginService', () => {
   it('Login service should be defined', () => {
     expect(service).toBeDefined();
   });
-
-
-  /*describe('sum()', () => {
-        it('Should add two numbers together', async () => {
-            expect(await service.sum(1,2)).toEqual(
-                3
-            )
-        });
-    })
-
-    describe('getName()', () => {
-        it('Should return a login entity', async () => {
-            expect(await service.getName()).toMatchObject(loginEntity);
-        });
-    })*/
-
-                       /* ADDED FOR DEMO 2 */
-
-
-    //expect(await service.addUser("lk@gmail.com", "acdvdf")).not.toBeNull;
-
-    //validate(email : string, password : string)
-    describe('validate()', () => {
-        it('Successfully validated details', async () => {
-            //expect(await service.validate("test@email.com","123")).not.toEqual(false);
-            expect(await service.validate("test@email.com","123")).toBeFalsy();
-        })
-    })
-
-    //getEntity_login(email : string, password : string)
-    describe('getEntity_login()', () => {
-        it('Successfully retrieved the login entity', async () => {
-            expect(await service.getEntity_login("test@email.com","123")).not.toBeNull();
-        })
-    })
-
-
 });
