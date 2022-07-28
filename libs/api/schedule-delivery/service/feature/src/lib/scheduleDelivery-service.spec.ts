3b15855297802cb385d5f539041db87afa8a6e10
lindzik <94564461+lindzik@users.noreply.github.com>
2022-07-28 19:23:41 +0200

    Create scheduleDelivery-service.spec.ts

---
+++ libs/api/schedule-delivery/service/feature/src/lib/scheduleDelivery-service.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { jest } from '@jest/globals'
+
+import { ScheduleDeliveryService } from './scheduleDelivery.service';
+import { ScheduleDeliveryEntity } from './scheduleDelivery.entity';
+
+import { ScheduleDeliveryRepository } from '@charity-spot/api/schedule-delivery/repository/feature';
+import { PrismaService } from '@charity-spot/api/shared/services/prisma';
+
+const scheduleDeliveryEntity = new ScheduleDeliveryEntity();
+
+describe('item Request Service', () => {
+
+    let service: ScheduleDeliveryService;
+    let repository: ScheduleDeliveryRepository;
+    let prisma: PrismaService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+          providers: [ScheduleDeliveryService, ScheduleDeliveryRepository, ScheduleDeliveryEntity, PrismaService],
+        }).compile();
+
+        service = module.get<ScheduleDeliveryService>(ScheduleDeliveryService);
+
+  });
+  it('item Request service should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
