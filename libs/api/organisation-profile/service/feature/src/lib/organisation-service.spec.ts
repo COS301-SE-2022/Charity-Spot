b7d2487a7ffb4d29a4908ebe061f43bb72f6a3f7
lindzik <94564461+lindzik@users.noreply.github.com>
2022-07-28 19:09:54 +0200

    Update organisation-service.spec.ts

--- libs/api/organisation-profile/service/feature/src/lib/organisation-service.spec.ts
+++ libs/api/organisation-profile/service/feature/src/lib/organisation-service.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { jest } from '@jest/globals'
 
 import { OrganisationService } from './organisation.service';
 import { OrganisationEntity } from './organisation.entity';
 
 import { OrganisationRepository } from '@charity-spot/api/organisation-profile/repository/data-access';
 import { PrismaService } from '@charity-spot/api/shared/services/prisma';
+import { identity } from 'rxjs';
 
 
 
@@ -18,7 +20,7 @@ describe ( 'Organisation Service', () => {
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-          providers: [OrganisationService, OrganisationRepository, PrismaService, OrganisationEntity],
+          providers: [OrganisationService, OrganisationRepository, OrganisationEntity, PrismaService],
         }).compile();
 
         service = module.get<OrganisationService>(OrganisationService);
@@ -27,29 +29,4 @@ describe ( 'Organisation Service', () => {
   it('Organisation service should be defined', () => {
     expect(service).toBeDefined();
   });
-
-
-  //getOrgProfile(userID: string)
-    describe('Get Organisation Profile', () => {
-        it('Should return Organisation Profile', async () => {
-          expect(await service.getOrgProfile("1")).toMatchObject(organisationEntity);
-        });        
-    })
-
-    //updateDet(id: string, name: string, loc: string, picture: string, password: string)
-    describe('Update Organisation Details', () => {
-      it('Should update an organisations details', async () => {
-        expect(await service.updateDet("1","test org","Pretoria","asdadwdadddd","12345")).toMatchObject(organisationEntity);
-      });
-    })
-
-
-    //getDonations(id: string)
-    describe('Get Donations', () => {
-      it('Should return null', async () => {
-        expect(await service.getDonations("1")).toBeNull();
-      });
-    })
-
-
-})
+});
