07da6e83ba4afca83b18e39fcababc346d0aa39b
lindzik <94564461+lindzik@users.noreply.github.com>
2022-07-28 19:16:13 +0200

    Update regist-service.spec.ts

--- libs/api/registration/service/feature/src/lib/regist-service.spec.ts
+++ libs/api/registration/service/feature/src/lib/regist-service.spec.ts
@@ -98,3 +98,18 @@ it('Alter NGO Num',async () => {
     expect(resolver.addUser).toHaveBeenCalled();
 
 });*/
+
+it('Adds an org',async () => {
+  jest
+    .spyOn(resolver,'addOrg')
+    //.mockImplementation( () => true);
+    //.mockImplementation( (): Promise<boolean> => boolean);
+    //.mockImplementation( (): Promise<string|null> => Promise.resolve("lk@gmail.com"));
+    .mockImplementation( (): Promise<string|null> => Promise.resolve("SUCCESS"));
+    expect(resolver.addOrg).not.toHaveBeenCalled();
+    //expect(await resolver.addUser("lk@gmail.com", "acdvdf")).toEqual(true);
+    expect(await resolver.addOrg("cl62huz0m0002f6ynu0z40get", "org 1")).toEqual("SUCCESS");
+    //expect(await resolver.addOrg("cl62huz0m0002f6ynu0z40get", "org 1")).toEqual("lk@gmail.com");
+    expect(resolver.addOrg).toHaveBeenCalled();
+
+});
