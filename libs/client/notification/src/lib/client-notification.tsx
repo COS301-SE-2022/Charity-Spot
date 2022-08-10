c60a2dfda22e5d5af15296ee6a77bc91d203c40f
Moloko <molomag.mm@gmail.com>
2022-08-10 21:49:02 +0200

    Adding notification window done

--- libs/client/notification/src/lib/client-notification.tsx
+++ libs/client/notification/src/lib/client-notification.tsx
@@ -1,5 +1,6 @@
 import styles from './client-notification.module.css';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { FaCheck, FaBinoculars } from 'react-icons/fa';
 import "./notifiii.css";
 /* eslint-disable-next-line */
 export interface ClientNotificationProps {}
@@ -11,16 +12,22 @@ export function ClientNotification(props: ClientNotificationProps) {
               <div className='notiMain'>
                 <div className='notiLeft'>
                   <br/><br/>
+                  {/* <div className="load"></div> */}
                 <img src="https://firebasestorage.googleapis.com/v0/b/cos301-storage-test.appspot.com/o/logo.png?alt=media&token=658a4502-2b08-47bf-8cb2-fe7eacbf8c3e" alt="" id="notiprofile-pic"></img>
                 
                 </div>
                 <div className='notiRight'>
                   <br></br>
-                  <div className='noticov'><h2>Nandos</h2></div>
-                  <div > <div className='noticov2'><h4>Location of Client: Pretoria</h4></div></div>
+                  
+                  <div > <div className='noticov2'>
+                    <div className='messageContent'>
+                    <div className='noticov'><h2>Nandos</h2></div>
+                    <h5 >Nandos sent a message</h5></div>
+                    </div>
+                    </div>
                   <div><h4></h4></div>
-                <Link to ='/chat' className='rgLink'><button id='notiMark'>Mark Read</button></Link>
-                <Link to ='/profile' className='rgLink'><button id='chatHistGo'>Detailed View</button></Link>
+                <button id='notiMark'>Mark Read <FaCheck/></button>
+                <button id='chatHistGo'>View <FaBinoculars/></button>
                 </div>
                 
               </div>
