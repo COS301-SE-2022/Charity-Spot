a09d9d736e67f764ec80292814af7e23f39368e5
David Roodt <davidroodt2@gmail.com>
2022-08-30 11:58:41 +0200

    AddRating

--- libs/api/comment-rating/repository/data-access/src/lib/commentRating.repository.ts
+++ libs/api/comment-rating/repository/data-access/src/lib/commentRating.repository.ts
@@ -5,4 +5,18 @@ import { PrismaService } from '@charity-spot/api/shared/services/prisma';
 export class CommentRatingRepository {
   constructor(private prisma: PrismaService) {}
 
+  async AddRatingNoCommnet(AssistID, NeedID : string, Rating : number)
+  {
+    const rating = await this.prisma.rating.create({
+      data:
+      {
+        OrgID: AssistID,
+        ClientID: NeedID,
+        rating: Rating
+      }
+    });
+
+    return rating;
+
+  }
 }
