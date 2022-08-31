48aa6842c5ac529f79c88b88357770ddedd5e284
lindzik <94564461+lindzik@users.noreply.github.com>
2022-08-31 12:31:28 +0200

    Update notification-service.spec.ts

--- libs/api/notification/service/feature/src/lib/notification-service.spec.ts
+++ libs/api/notification/service/feature/src/lib/notification-service.spec.ts
@@ -8,3 +8,16 @@ import { NotificationEntity } from './notification.entity';
 import { NotificationRepository } from '@charity-spot/api/notification/repository/feature'
 import { PrismaService } from '@charity-spot/api/shared/services/prisma';
 import { async } from '@firebase/util';
+
+
+const notificationEntity = new NotificationEntity();
+
+describe ( 'Notification Service', () => {
+    let service: NotificationService;
+    let repository: NotificationRepository;
+    let prisma: PrismaService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+          providers: [NotificationService, NotificationRepository,NotificationEntity, PrismaService],
+        }).compile();
