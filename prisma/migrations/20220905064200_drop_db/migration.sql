9196c39862e5480476cce66f1b49a339b3dfa119
David Roodt <davidroodt2@gmail.com>
2022-09-05 08:44:47 +0200

    drop DB

---
+++ prisma/migrations/20220905064200_drop_db/migration.sql
@@ -0,0 +1,38 @@
+/*
+  Warnings:
+
+  - You are about to drop the `address` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `chat_history` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `delivery` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `donation_item` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `organisation` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `rating` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "address";
+
+-- DropTable
+DROP TABLE "chat_history";
+
+-- DropTable
+DROP TABLE "delivery";
+
+-- DropTable
+DROP TABLE "donation_item";
+
+-- DropTable
+DROP TABLE "organisation";
+
+-- DropTable
+DROP TABLE "rating";
+
+-- DropTable
+DROP TABLE "user";
+
+-- DropEnum
+DROP TYPE "catagory";
+
+-- DropEnum
+DROP TYPE "quality";
