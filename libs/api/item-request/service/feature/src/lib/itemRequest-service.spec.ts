412f318b302cb6d511970efd232aa15a2f5f5df3
lindzik <94564461+lindzik@users.noreply.github.com>
2022-09-25 22:17:56 +0200

    Update itemRequest-service.spec.ts

--- libs/api/item-request/service/feature/src/lib/itemRequest-service.spec.ts
+++ libs/api/item-request/service/feature/src/lib/itemRequest-service.spec.ts
@@ -1,3 +1,4 @@
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { jest } from '@jest/globals'
 
@@ -6,121 +7,35 @@ import { itemRequestEntity } from './itemRequest.entity';
 
 import { itemRequestRepository } from '@charity-spot/api/item-request/repository/feature';
 import { PrismaService } from '@charity-spot/api/shared/services/prisma';
+import { direct } from '@charity-spot/api/shared/auth';
+describe( 'Comment Rating Service', () => {
 
-const ItemRequestEntity = new itemRequestEntity();
+    let service : itemRequestService;
+    let repository : itemRequestRepository;
 
-describe('item Request Service', () => {
-
-    let service: itemRequestService;
-    let repository: itemRequestRepository;
-    let prisma: PrismaService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-          providers: [itemRequestService, itemRequestRepository, itemRequestEntity, PrismaService],
+          providers: [itemRequestService, PrismaService, itemRequestRepository],
         }).compile();
 
         service = module.get<itemRequestService>(itemRequestService);
-
-  });
-  it('item Request service should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
-
-let resolver: itemRequestService;
-
-beforeEach(async () => {
-  const module: TestingModule = await Test.createTestingModule({
-    providers: 
-    [itemRequestService, 
-      itemRequestRepository,
-      itemRequestEntity, 
-      PrismaService, 
-    ],
-  }).compile();
-  resolver = module.get<itemRequestService>(itemRequestService);
-});
-
-// PART 1 - START
-
-//Test()
-it('Tests', async () => {
-    jest
-        .spyOn(resolver,'Test')
-        .mockImplementation((): Promise<itemRequestEntity> => Promise.resolve(ItemRequestEntity));
-        expect(resolver.Test).not.toHaveBeenCalled();
-        expect(await resolver.Test()).toMatchObject(ItemRequestEntity);
-        expect(resolver.Test).toHaveBeenCalled();
-});
-
-//getAIPredic(Date : string, itemType : string, location : string)
-it('Predicts the AI', async () => {
-    jest
-        .spyOn(resolver,'getAIPredic')
-        .mockImplementation((): Promise<itemRequestEntity> => Promise.resolve(ItemRequestEntity));
-        expect(resolver.getAIPredic).not.toHaveBeenCalled();
-        expect(await resolver.getAIPredic("28-07-2022 08:57","Clothing","Pretoria")).toMatchObject(ItemRequestEntity);
-        expect(resolver.getAIPredic).toHaveBeenCalled();
-
-});
-
-// PART 1 - END
-
-
-// PART 2 - START
-
-const record = {testDate:"28-07-2022 08:57", testitemType:"Clothing",testLocation:"Pretoria",testId:"item Request Working!"};
-
-export class itemRequestServiceMock
-    {
-        Test()
-            {
-                const ret = new itemRequestEntity();
-                ret.ID = "item Request Working!";
-
-                return ret;
-            }
-
-        getAIPredic(Date : string, itemType : string, location : string)
-            {
-                const ret = new itemRequestEntity();
-                ret.ID = "item Request Working!";
-
-                return ret;
-            }
-    }
-
-// PART 2 - END
-
-
-// PART 3 - START
-
-const servicemock = new itemRequestServiceMock();
-
-describe('item Request Service Mock', () => {
-    //Test()
-    describe('Test()', () => {
-        it('Should successfully test service', async () => {
-            const expectedVal = new itemRequestEntity();
-            expectedVal.ID = record.testId;
-            const val = servicemock.Test();
-            expect(expectedVal).toMatchObject(val);
-        }); 
-    });
-    
-    //getAIPredic(Date : string, itemType : string, location : string)
-    describe('getAIPredic', () => {
-        it('Should successfully predict using the AI', async () => {
-            const expectedVal = new itemRequestEntity();
-            expectedVal.ID = record.testId;
-            const val = servicemock.getAIPredic(record.testDate,record.testitemType,record.testLocation);
-            expect(expectedVal).toMatchObject(val);
-        }); 
+        repository = module.get<itemRequestRepository>(itemRequestRepository);
     });
 
- 
-    
+  
+   //FindOrgInfo(OrgID)
+   describe('FindOrgInfo', () => {
+    it('Finds an organisations information', async () => {
+        jest.spyOn(service, 'FindOrgInfo').mockImplementation(() : Promise<any> => Promise.resolve(true));
+        expect(await service.FindOrgInfo("sda1w44")).toEqual(true);
+    })
 });
 
-// PART 3 - END
+   //getAIPredic(Date : string, itemType : string, location : string)
+   describe('getAIPredic', () => {
+    it('Gets the AIs predictions', async () => {
+        jest.spyOn(service, 'getAIPredic').mockImplementation(() : Promise<any> => Promise.resolve(true));
+        expect(await service.getAIPredic("01-03-2022","Jeans","1234 Pretoria Street")).toEqual(true);
+    })
+});
