1908b7513d6d29d8c473a43487c4977983ff6836
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-09-25 18:54:05 +0200

    Working on integration tests

--- apps/client-e2e/src/integration/app.spec.ts
+++ apps/client-e2e/src/integration/app.spec.ts
@@ -1,190 +1,150 @@
 describe('testing client', () => {
 
-    Cypress.Cookies.defaults({
-        preserve: ['ID', 'ID_EXT', 'foreignID'],
-      });
+    //save cookies between tests
 
-    //login page
-    it('should direct to the right url', () => {
-        cy.visit('http://3.7.189.9:8082/login');
-        cy.url().should('include','login');
-    })
-
-    it('The user should be able to login', () =>{
-
-        cy.get('input[type=email]').type("client1@email.com");
-        cy.get('input[type=password]').type("1234");
-
-        cy.contains('Log in').click();
-    })
-
-
-    //home page
-    it('should direct to the right url', () => {
-        cy.url().should('include','home');
-    })
-
-    it('should be taken to profile page of org', () => {
-
-        cy.get('[title="PEP"] > img').click();
-
-    });
-
-
-    //profile page
-    it('should direct to the right url', () => {
-        cy.url().should('include','profile');
-    })
-
-    it('should be taken to profile page of org', () => {
-
-        cy.get('.blog').click();
-
-    });
-
-    it('should be able to view item', () => {
-        cy.get(':nth-child(1) > .rapper > .collapsible > label').click();
-
-        cy.get('#P10pic').should('be.visible');
-        
-    })
+      beforeEach(function () {
+        cy.getCookies().then(cookies => {
+          const namesOfCookies = cookies.map(c => c.name)
+          Cypress.Cookies.preserveOnce(...namesOfCookies)
+        })
+      })
 
     
+    //client test
 
-});
-
-    /*//registration page
-    describe('registration testing', () => {
-        beforeEach(() =>cy.visit('http://localhost:4200/register'));
+        //login page
         it('should direct to the right url', () => {
-            cy.url().should('include','register');
-        })
-
-        it('The user should be able to Register', () =>{
-            cy.visit('http://localhost:4200/register')
-            
-            cy.get('#rgorgnm1').select('Organization').should('have.value','Organization');
-
-            cy.get('#rgorgnm2').type("Organisation 1")
-            cy.get('input[type=email]').type("test@gmail.com");
-            cy.get('#lct1').type("Pretoria");
-            cy.get('#rgpwd1').type("1234");
-            cy.get('#rgpwd2').type("1234");
-            
-            //cy.contains('Register').click();
-            cy.get('#rgsub_butt').click();
-        })
-
-    });
-
-
-    //login page
-    describe('login page testing', () => {
-        //beforeEach(() =>cy.visit('/login-page'));
-        beforeEach(() =>cy.visit('http://localhost:4200/login'));
-        it('should direct to the right url', () => {
+            cy.visit('http://3.7.189.9:8082/login');
             cy.url().should('include','login');
         })
 
-    it('The user should be able to login', () =>{
-        //cy.visit('http://localhost:4200/login')
+        it('The user should be able to login', () =>{
 
-        cy.get('input[type=email]').type("test@gmail.com");
-        cy.get('input[type=password]').type("1234");
+            cy.get('input[type=email]').type("client1@email.com");
+            cy.get('input[type=password]').type("1234");
 
-        cy.contains('Log in').click();
-
-    })
-
-});
-    
-    //donation page
-    describe('donate page testing', () => {
-        beforeEach(() => cy.visit('http://localhost:4200/donate'));
-        it('should direct you to the right url', () => {
-            cy.url().should('include','donate');
-
-        it('The user should be able to donate an item', () => {
-            cy.get('.din1').type('Jackets');//Name
-            cy.get('.din2').type('4');//Quantity
-            //cy.get('select').select('Clothing').should('have.value','Clothes');//Food Item
-            cy.get('.din3').select('Clothing').should('have.value','Clothes');//Food Item
-            cy.get('.din4').select('Used').should('have.value','Used');//Condition
-            cy.get('.din5').type('Consists of 1 pink,1 black,1 green and 1 blue jacket');//Description
-            cy.get('#dnt_but').click();
+            cy.contains('Log in').click();
         })
 
+
+        //home page
+        it('should direct to the right url', () => {
+            cy.url().should('include','home');
         })
-    });
 
-    /* ADDED FOR DEMO 3*/
+        it('should be taken to profile page of org', () => {
 
-    //profile page
-    /*describe('profile page testing', () => {
-        beforeEach(() => cy.visit('http://localhost:4200/profile'));
-        it('should direct you to the right url', () => {
+            cy.get('[title="charity1"] > img').click();
+
+        });
+
+
+        //profile page
+        it('should direct to the right url', () => {
             cy.url().should('include','profile');
+        })
 
-        it('The user should be able to view an organisations profile page', () => {
+        it('should be taken to profile page of org', () => {
+
+            cy.get('.blog').click();
+
+        });
+
+        it('should be able to view item', () => {
+            cy.get(':nth-child(1) > .rapper > .collapsible > label').click();
+
+            //cy.get('#P10pic').should('be.visible');
+            
+        })
+
+        it('should navigate back to profile page', () => {
+            cy.get('.profTab').click();
+        })
+
+
+        //chat test
+        it('should open a chat window', () => {
             cy.get('#chatGo').click();
-        })
-
-        })
-    });
-
-
-    //chat page
-    describe('chat page testing', () => {
-        beforeEach(() => cy.visit('http://localhost:4200/chat'));
-        it('should direct you to the right url', () => {
             cy.url().should('include','chat');
+        
+        })
 
-        it('The user should be able to view the chat area', () => {
-            //cy.get('#chatGo').click();
-            cy.get('.client-chat_myText__3vWpw').type("Hello i am testing the chat feature");
+        it('should send message', () => {
+
+            cy.get('#inputBoxM').type("hello world");
             cy.get('.client-chat_sndBT__3serM').click();
-        })
+
+        });
+
+        it('should navigate to active char sessions', () => {
+
+            cy.get('[data-rr-ui-event-key="chatSession"]').click();
 
         })
-    });
 
-    //Schedule Delivery page
-    describe('Schedule Delivery page testing', () => {
-        beforeEach(() => cy.visit('http://localhost:4200/scheduleDelivery'));
-        it('should direct you to the right url', () => {
+    //charity test
+
+        it('should be logged out of client profile', () =>{
+
+            cy.visit('http://3.7.189.9:8082/login');
+            cy.url().should('include','login');
+
+        });
+
+        it('charity should be able to login', () =>{
+
+            cy.get('input[type=email]').type("charity1@email.com");
+            cy.get('input[type=password]').type("1234");
+
+            cy.contains('Log in').click();
+        });
+
+        //home page
+        it('should direct to the right url', () => {
+            cy.url().should('include','home');
+        })
+
+        //notifications
+        it('should be able to view notifications', () => {
+
+            cy.get('#notButton > svg > path').click();
+            cy.get('#chatHistGo').click();
+
+        });
+
+        //donate
+        it('should direct to the right url', () => {
+            cy.url().should('include','chat');
+        })
+
+        it('should be able to navigate to profile', () => {
+            cy.get('#chatNameP').click();
+        })
+
+        it('should direct to the right url', () => {
+            cy.url().should('include','profile');
+        })
+
+        it('should be able to navigate to profile', () => {
+            cy.get('#delivGo').click();
+        })
+
+        it('should direct to the right url', () => {
             cy.url().should('include','scheduleDelivery');
+        })
 
-        it('The user should be able to schedule a delivery', () => {
-            cy.contains('Your Name').type('Steve');  
-            cy.contains('Recipient Name').type('Dave');
-            cy.contains('Hatfield').type('Pretoria');
-            cy.contains('Polokwane').type('Limpopo');
+        it('should be able to confirm', () => {
             cy.get('#deliv_but').click();
         })
 
-        })
-    });
+        //donate schedule
 
-    //Item Request Page
-    describe('Item Request page testing', () => {
-        beforeEach(() => cy.visit('http://localhost:4200/itemRequest'));
-        it('should direct you to the right url', () => {
-            cy.url().should('include','itemRequest');
-
-        it('The user should be able to request an item', () => {
-            cy.contains('Item Name').type('Jeans');  
-            cy.contains('Item(s) Quantity').type('3');
-            cy.contains('Your Location').type('Pretoria');
-            
-            cy.get('.req3').select('Clothing').should('have.value','Clothes');
-            cy.contains('Request').click();
+        it('should be able to navigate to profile', () => {
+            cy.get('[data-rr-ui-event-key="donationSchedule"]').click();
         })
 
+        it('should direct to the right url', () => {
+            cy.url().should('include','donationSchedule');
         })
-    });
 
-/*cy.get('id') - by tag
-cy.get('.id') - by class 
-cy.get('#id')- by id*/
-
-//});
+});
