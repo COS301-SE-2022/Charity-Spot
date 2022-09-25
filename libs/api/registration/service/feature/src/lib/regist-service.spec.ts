5c5de82b02ec224009da606e2c612243d5fdfcc3
lindzik <94564461+lindzik@users.noreply.github.com>
2022-09-25 22:23:53 +0200

    Update regist-service.spec.ts

--- libs/api/registration/service/feature/src/lib/regist-service.spec.ts
+++ libs/api/registration/service/feature/src/lib/regist-service.spec.ts
@@ -11,268 +11,33 @@ import { RegistrationRepository } from '@charity-spot/api/registration/repositor
 import { PrismaService } from '@charity-spot/api/shared/services/prisma';
 import { Console } from 'console';
 
+describe( 'Registration Service', () => {
 
+  let service : RegistrationService;
+  let repository : RegistrationRepository;
 
-const RegistrationEntity = new RegistEntity();
-
-describe ( 'Registration Service', () => {
-    let service: RegistrationService;
-    let repository: RegistrationRepository;
-    let prisma: PrismaService;
-
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-          providers: [RegistrationService, RegistrationRepository,RegistEntity, PrismaService, LoginService, LoginRepository],
-        }).compile();
-
-        service = module.get<RegistrationService>(RegistrationService);
-
-  });
-  it('Registration service should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
-
-  /*let resolver: RegistrationService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: 
-      [RegistrationService, 
-        RegistrationRepository,
-        RegistEntity, 
-        PrismaService, 
-        LoginService, 
-        LoginRepository
-      ],
-    }).compile();
-    resolver = module.get<RegistrationService>(RegistrationService);
+      const module: TestingModule = await Test.createTestingModule({
+        providers: [RegistrationService, PrismaService, RegistrationRepository],
+      }).compile();
+
+      service = module.get<RegistrationService>(RegistrationService);
+      repository = module.get<RegistrationRepository>(RegistrationRepository);
   });
 
-/*
-it('',async () => {
-  jest
-    .spyOn(resolver,'')
-    .mockImplementation( () => );
-    expect(resolver.).not.toHaveBeenCalled();
-
-    expect(resolver.).toHaveBeenCalled();
-
-});
-*/
-
-/*it('Org does not exist',async () => {
-  jest
-    .spyOn(resolver,'doesNotExist')
-    .mockImplementation( (): Promise<boolean> => Promise.resolve(true));
-    expect(resolver.doesNotExist).not.toHaveBeenCalled();
-    expect(await resolver.doesNotExist("lk@gmail.com", "acdvdf")).toEqual(true);
-    expect(resolver.doesNotExist).toHaveBeenCalled();
-
+	//regClient(id: string, flier: string, pin: string, hack: string)
+	describe('regClient', () => {
+    it('Registers a client', async () => {
+        jest.spyOn(service, 'regClient').mockImplementation(() : Promise<any> => Promise.resolve(true));
+        expect(await service.regClient("asdw525wd","12456322","542412","awjkdajk45")).toEqual(true);
+    })
 });
 
-it('Adds user',async () => {
-  jest
-    .spyOn(resolver,'addUser')
-    .mockImplementation( (): Promise<string|null> => Promise.resolve("lk@gmail.com"));
-    expect(resolver.addUser).not.toHaveBeenCalled();
-    expect(await resolver.addUser("lk@gmail.com", "acdvdf")).toEqual("lk@gmail.com");
-    expect(resolver.addUser).toHaveBeenCalled();
-
+  //regOrg(badge: string, relay: string, rendezvous: string, riddle: string)
+  describe('regOrg', () => {
+    it('Registers an Organisation', async () => {
+        jest.spyOn(service, 'regOrg').mockImplementation(() : Promise<any> => Promise.resolve(true));
+        expect(await service.regOrg("awdadawda4a4","1254","5454544s","wda455wd45w")).toEqual(true);
+    })
 });
-
-
-/* */
-/*
-it('Alter NGO Num',async () => {
-  jest
-    .spyOn(resolver,'alterNGONum')
-    //.mockImplementation( () => true);
-    //.mockImplementation( (): Promise<boolean> => boolean);
-    //.mockImplementation( (): Promise<string|null> => Promise.resolve("lk@gmail.com"));
-    .mockImplementation( (): Promise<string> => Promise.resolve("lk@gmail.com"));
-    expect(resolver.addUser).not.toHaveBeenCalled();
-    //expect(await resolver.addUser("lk@gmail.com", "acdvdf")).toEqual(true);
-    expect(await resolver.addUser("lk@gmail.com", "acdvdf")).toEqual("lk@gmail.com");
-    expect(resolver.addUser).toHaveBeenCalled();
-
-});*/
-
-/*it('Adds an org',async () => {
-  jest
-    .spyOn(resolver,'addOrg')
-    //.mockImplementation( () => true);
-    //.mockImplementation( (): Promise<boolean> => boolean);
-    //.mockImplementation( (): Promise<string|null> => Promise.resolve("lk@gmail.com"));
-    .mockImplementation( (): Promise<string|null> => Promise.resolve("SUCCESS"));
-    expect(resolver.addOrg).not.toHaveBeenCalled();
-    //expect(await resolver.addUser("lk@gmail.com", "acdvdf")).toEqual(true);
-    expect(await resolver.addOrg("cl62huz0m0002f6ynu0z40get", "org 1")).toEqual("SUCCESS");
-    //expect(await resolver.addOrg("cl62huz0m0002f6ynu0z40get", "org 1")).toEqual("lk@gmail.com");
-    expect(resolver.addOrg).toHaveBeenCalled();
-
-});
-
-it('Adds a picture',async () => {
-  jest
-    .spyOn(resolver,'addPicture')
-    //.mockImplementation( () => true);
-    //.mockImplementation( (): Promise<boolean> => boolean);
-    //.mockImplementation( (): Promise<string|null> => Promise.resolve("lk@gmail.com"));
-    .mockImplementation( () => null);
-    expect(resolver.addPicture).not.toHaveBeenCalled();
-    //expect(await resolver.addUser("lk@gmail.com", "acdvdf")).toEqual(true);
-    expect(await resolver.addPicture("cl62huz0m0002f6ynu0z40get","org 1","sadwdad")).toBeNull();
-    //expect(await resolver.addOrg("cl62huz0m0002f6ynu0z40get", "org 1")).toEqual("lk@gmail.com");
-    expect(resolver.addPicture).toHaveBeenCalled();
-
-});
-
-
-const record = {testName:"org 1",testEmail:"lk@gmail.com",testPass: "acdvdf",testid: "cl62huz0m0002f6ynu0z40get",testOrgNum:"1",testDesc:" this is a test description for org 1",testAdress:"123 test Address 1"};
-const recordUpdate = {testName:"org 2",testEmail:"lk@gmail.com",testPass: "acdvdf",testid: "cl62huz0m0002f6ynu0z40get",testOrgNum:"1",testDesc:" this is a test description for org 2",testAdress:"456 test Address 2"};
-
-export class RegistrationServiceMock
-  {
-    doesNotExist(email : string, password : string)
-      {
-        if(email.match(record.testEmail) && (password.match(record.testPass)))
-          {
-            return false;
-          }
-        else
-          {
-            return true;
-          }
-      }
-
-      addUser(email : string, password : string)
-        {
-          if(this.doesNotExist(email,password) == true) 
-            {
-              return record.testid;
-            }
-          else
-            {
-              return null;
-            }
-        }
-
-      alterNGONum(UserID : string, NGONum: string)
-        {
-          if(UserID.match(recordUpdate.testid) && (NGONum.match(recordUpdate.testOrgNum)))
-            {
-              recordUpdate.testOrgNum = NGONum;
-              return "SUCCESS";
-            }
-          else
-            {
-              return "FAILURE";
-            }
-          
-        }
-
-      alterDescr(userID : string, Descr : string)
-        {
-          if(userID.match(recordUpdate.testid) && (Descr.match(recordUpdate.testDesc)))
-            {
-              recordUpdate.testDesc = Descr;
-              return "SUCCESS";
-            }
-          else
-            {
-              return "FAILURE";
-            }
-        }
-
-      alterAddress( userID : string, address: string, address2: string, city: string, prov: string )  
-        {
-          if(userID.match(recordUpdate.testid))
-            {
-              recordUpdate.testAdress = address;
-              return "SUCCESS";
-            }
-          else
-            {
-              return "FAILURE";
-            }
-        }
-
-      addOrg(userID : string, OrgName : string)
-        {
-          return "SUCCESS";
-        }
-
-      addPicture(id: string, name: string, picture: string)
-        {
-          return null;
-        }
-  }
-
-const servicemock = new RegistrationServiceMock();
-
-describe('Registration Service Mock', () => 
-  {
-    describe('Org does not exist', () => {
-      it('The Organisation should not already exist', async () => {
-        const expectedVal = false;
-        const val = servicemock.doesNotExist(record.testEmail,record.testPass);
-        expect(expectedVal).toBe(val);
-      }); 
-    });
-    
-    describe('Adding User', () => {
-      it('A new User should be added', async () => {
-        //const expectedVal = record.testid;
-        const val = servicemock.addUser("lk@gmail.com", "acdvdf");
-        expect(val).not.toBeNull;
-      }); 
-    });
-
-
-
-      //alterNGONum("1","125dawdf")
-describe('Altering the NGO number', () => {
-  it('Successfully altered the NGO number', async () => {
-    const expectedVal = servicemock.alterNGONum("cl62huz0m0002f6ynu0z40get","1");
-    expect(expectedVal).toEqual(expect.any(String));
-  }); 
-});
-
-//alterDescr("1","This is an NGO that supplies food and clothes")
-describe('Altering the description', () => {
-  it('Successfully altered the description', async () => {
-    const expectedVal = servicemock.alterDescr("cl62huz0m0002f6ynu0z40get","This is an NGO that supplies food and clothes");
-    expect(expectedVal).toEqual(expect.any(String));
-  }); 
-});
-
-//alterAddress("1","1234 Park Address","5678 Konoha Street","Pretoria","Gauteng")
-describe('Altering the address', () => {
-  it('Successfully altered the address', async () => {
-    const expectedVal = servicemock.alterAddress("cl62huz0m0002f6ynu0z40get","1234 Park Address","5678 Konoha Street","Pretoria","Gauteng");
-    expect(expectedVal).toEqual(expect.any(String));
-  }); 
-});
-
-//addOrg("5","An incredible Org")
-describe('Adding a new organisation', () => {
-  it('Successfully added a new organisation', async () => {
-    const expectedVal = servicemock.addOrg("cl62huz0m0002f6ynu0z40get","An incredible Org");
-    console.log(expectedVal);
-    expect(expectedVal).toEqual("SUCCESS");
-  }); 
-});
-
-
-
-//addPicture(id: string, name: string, picture: string)
-describe('Adding a new picture', () => {
-  it('Successfully added picture', async () => {
-    const expectedVal = servicemock.addPicture("cl62huz0m0002f6ynu0z40get","org 1","sadwdad");
-    expect(expectedVal).toBeNull();
-  });
-});
-
-  });*/
-
