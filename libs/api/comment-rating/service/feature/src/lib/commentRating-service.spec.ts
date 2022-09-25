413866c46e4609f5acb33d5cc3f3c93d2a87ab03
lindzik <94564461+lindzik@users.noreply.github.com>
2022-09-25 22:12:25 +0200

    Create commentRating-service.spec.ts

---
+++ libs/api/comment-rating/service/feature/src/lib/commentRating-service.spec.ts
@@ -0,0 +1,41 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { CommentRatingService } from './commentRating.service';
+import { CommentRatingEntity } from './commentRating.entity';
+
+import { CommentRatingRepository } from '@charity-spot/api/comment-rating/repository/data-access';
+import { PrismaService } from '@charity-spot/api/shared/services/prisma';
+
+import { direct } from '@charity-spot/api/shared/auth';
+
+describe( 'Comment Rating Service', () => {
+
+    let service : CommentRatingService;
+    let repository : CommentRatingRepository;
+
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+          providers: [CommentRatingService, PrismaService, CommentRatingRepository],
+        }).compile();
+
+        service = module.get<CommentRatingService>(CommentRatingService);
+        repository = module.get<CommentRatingRepository>(CommentRatingRepository);
+    });
+
+    // getAllInfo(AssistID: string)
+    describe('getAllInfo', () => {
+        it('Gets all the information', async () => {
+            jest.spyOn(service, 'getAllInfo').mockImplementation(() : Promise<any> => Promise.resolve(true));
+            expect(await service.getAllInfo("a1d4f5g96")).toEqual(true);
+        })
+    });
+    
+    //getAverageRatings(ratings : number[])
+    describe('getAverageRatings', () => {
+        it('Gets the average ratings', async () => {
+            jest.spyOn(service, 'getAverageRatings').mockImplementation(() : Promise<any> => Promise.resolve(true));
+            const val = [1,4,3,2];
+            expect(await service.getAverageRatings(val)).toEqual(true);
+        })
+    });
