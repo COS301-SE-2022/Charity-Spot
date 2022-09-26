0921fe8104177c07f62e81ebee0b407e1c9cacfa
Timo van der Merwe <timovandermerwe@Timos-MacBook-Air.local>
2022-09-26 11:04:45 +0200

    Working on performance test

---
+++ apps/client-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message)
+      
+          return null
+        },
+      })
+    }
+  }
+})
